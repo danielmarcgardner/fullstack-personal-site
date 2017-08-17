26e27db2c1075bfb47601b08fac2ba28754159d9
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-08-16 18:07:47 -0700

    Finish Experience section.

--- personal-site-front-end/src/Components/Home/Home.js
+++ personal-site-front-end/src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroDiv from '../HeroDiv/HeroDiv';
 import SkillsHero from '../Skills/SkillsHero';
+import Experience from '../Experience/Experience';
 
 const Home = () => (
   <div>
     <HeroDiv />
+    <Experience />
     <SkillsHero />
   </div>
 );
